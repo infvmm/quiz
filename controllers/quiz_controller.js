8700bc587d0e898303489a036d4c1c4e89c8ed87
victor <infvmm00@gmail.com>
2015-07-14 22:53:45 +0200

    test

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -27,16 +27,22 @@ var models = require('../models/models.js');
 // GET /quizes
 exports.index = function(req, res){
   var param = req.query.search;
+  console.log('index' + param);
     if(param) {
         var search = param.trim();
         search = '%' + search + '%';
         search = search.replace(/\s+/g, '%');
+        console.log('index search' + search);
         models.Quiz.findAll({
                 where: ["pregunta like ?", search], 
                 order: '`pregunta` ASC'
               }).then(function(quizes){
+                console.log('index quizes' + quizes);
         res.render('quizes/index', {quizes:quizes});
-        }).catch(function(error){next(error);});
+        }).catch(function(error){
+          console.log('index error' + error);
+          next(error);
+        });
     } else {
         res.render('quizes/index', {quizes:undefined});
     }
@@ -44,14 +50,14 @@ exports.index = function(req, res){
 
 // Autoload - factoriza el codigo si ruta incluye :quizId
 exports.load = function(req, res, next, quizId){
-  console.load('quizId' + quizId);
+  console.log('quizId' + quizId);
   models.Quiz.find(quizId).then(
       function(quiz){
-        console.load('quiz' + quiz);
+        console.log('quiz' + quiz);
         if(quiz){
-          console.load('quiz' + quiz.id);
+          console.log('quiz' + quiz.id);
           req.quiz = quiz;
-          console.load('quiz next' );
+          console.log('quiz next' );
           next();
         } else {
            next(new Error('No existe quizId: ' + quizId));
@@ -62,8 +68,8 @@ exports.load = function(req, res, next, quizId){
 
 // GET /quizes/:id
 exports.show = function(req, res) {
-  console.load('show' + req.quiz.id);
-  console.load('show' + req.quiz.id);
+  console.log('show' + req.quiz.id);
+  console.log('show' + req.quiz.id);
     res.render('quizes/show', {quiz: req.quiz});
 };
 
