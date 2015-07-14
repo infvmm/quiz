73a3f3f5383b4cfaef9c0eb942bc05ec24ceebed
victor <infvmm00@gmail.com>
2015-07-14 23:11:53 +0200

    Eliminacion de logs

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -27,22 +27,18 @@ var models = require('../models/models.js');
 // GET /quizes
 exports.index = function(req, res){
   var param = req.query.search;
-  console.log('index param: ' + param);
     if(param) {
         var search = param.trim();
         search = '%' + search + '%';
         search = search.replace(/\s+/g, '%');
-        console.log('index search: ' + search);
         models.Quiz.findAll(
                 {
                    where: ["pregunta like ?", search], 
                    order: 'pregunta ASC'
                  }
               ).then(function(quizes){
-                console.log('index quizes: ' + quizes);
         res.render('quizes/index', {quizes:quizes});
         }).catch(function(error){
-          console.log('index error: ' + error);
           next(error);
         });
     } else {
@@ -52,14 +48,10 @@ exports.index = function(req, res){
 
 // Autoload - factoriza el codigo si ruta incluye :quizId
 exports.load = function(req, res, next, quizId){
-  console.log('quizId' + quizId);
   models.Quiz.find(quizId).then(
       function(quiz){
-        console.log('quiz' + quiz);
         if(quiz){
-          console.log('quiz' + quiz.id);
           req.quiz = quiz;
-          console.log('quiz next' );
           next();
         } else {
            next(new Error('No existe quizId: ' + quizId));
@@ -70,8 +62,6 @@ exports.load = function(req, res, next, quizId){
 
 // GET /quizes/:id
 exports.show = function(req, res) {
-  console.log('show' + req.quiz.id);
-  console.log('show' + req.quiz.id);
     res.render('quizes/show', {quiz: req.quiz});
 };
 
