84abe53858aacd0ec91158dead929044c4bf88c5
victor <infvmm00@gmail.com>
2015-07-22 21:12:52 +0200

    Añadir funcionalidad de comentarios

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -53,7 +53,10 @@ exports.index = function(req, res){
 
 // Autoload - factoriza el codigo si ruta incluye :quizId
 exports.load = function(req, res, next, quizId){
-  models.Quiz.find(quizId).then(
+  models.Quiz.find({
+      where: { id: Number(quizId)},
+      include: [{model: models.Comment}]
+  }).then(
       function(quiz){
         if(quiz){
           req.quiz = quiz;
--- models/models.js
+++ models/models.js
@@ -31,7 +31,15 @@ var sequelize = new Sequelize(DB_name, user, pwd, {
 // Importar la definicion de la tabla Quiz en quiz.js
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 
+// Importar la definicion de la tabla Comment en comment.js
+var Comment = sequelize.import(path.join(__dirname,'comment'));
+
+// Relacion entre tabla comment y quiz
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
+
 exports.Quiz = Quiz; // Exportar definicion de la tabla Quiz
+exports.Comment = Comment; // Exportar definicion de la tabla Comment
 
 // sequelize.sync() crear e inicializa tabla de preguntas en DB
 sequelize.sync().then(function(){
